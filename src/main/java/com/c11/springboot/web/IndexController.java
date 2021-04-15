6256e73f6a593279539f1206ebacdb5ac5eeb063
Pete Lee <welikeheon@gmail.com>
2021-04-16 07:15:23 +0900

    Applied Post Save with ajax

--- src/main/java/com/c11/springboot/web/IndexController.java
+++ src/main/java/com/c11/springboot/web/IndexController.java
@@ -5,9 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-    // 머스테치 스타터로 인해 문자열 반환할 때 앞의 경로와 뒤의 파일 확장자가 자동으로 지정됨.
     @GetMapping("/")
     public String index() {
+        // 머스테치 스타터로 인해 문자열 반환할 때 앞의 경로와 뒤의 파일 확장자가 자동으로 지정됨.
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
